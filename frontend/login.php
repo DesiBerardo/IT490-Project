7eb4e80b5670850620c01d7c585426900aecf5ef
Yash Patel <yashppatel01@gmail.com>
2023-02-06 21:27:42 -0500

    barebones login page with display

--- frontend/login.php
+++ frontend/login.php
@@ -1,36 +1,18 @@
+<html>
+<body>
+
 <?php
+$username = $_POST["username"] ;
+$password = $_POST["password"];
+
+echo $username;
+echo $password;
 ?>
-<form onsubmit="return validate(this)" method="POST">
-    <div>
-        <label for="email">Email Address</label>
-        <input type="text" name="email" required />
-    </div>
-    <div>
-        <label for="pw">Password</label>
-        <input type="password" id="pw" name="password" required minlength="8" />
-    </div>
-    <input type="submit" value="Login" />
-</form>
-<script>
-    function validate(form) {
-       
-        let isValid = true ;
-        if((form.email.value).length > 0){ 
 
-            var emailConditions = /\S+@\S+\.\S+/ ;
-            if (!emailConditions.test(form.email.value)){
+<h3> Welcome <?php echo $_POST["name"]; ?> </h3><br>
+<h3>Your email address is: <?php echo $_POST["email"]; ?></h3>
 
-                isValid = false; flash("Sorry, that is an invalid email address. Please try again!");
+</body>
+</html>
 
-            }
-        
-        } else { isValid = false; flash("Please fill out a valid email address. Please try again!");}
 
-        if((form.password.value).length < 8){ 
-            isValid = false; flash("Password is too short. Please enter more characters.");
-        }
-        
-        return isValid;
-        
-    }
-</script>
