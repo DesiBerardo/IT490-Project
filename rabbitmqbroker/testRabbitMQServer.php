f891f1154121610a4f67f82ab4159472be853983
IT490 <desiberardo@yahoo.com>
2023-02-08 12:50:05 -0500

    Server will recieve, need to be sure I can get response from sql database

--- rabbitmqbroker/testRabbitMQServer.php
+++ rabbitmqbroker/testRabbitMQServer.php
@@ -6,9 +6,40 @@ require_once('rabbitMQLib.inc');
 
 function doLogin($username,$password)
 {
-    // lookup username in databas
+    // lookup username in database
+    $client = new rabbitMQClient("rabbitmqdb.ini","testServer");
+    if (isset($argv[1]))
+    {
+      $msg = $argv[1];
+    }
+    else
+    {
+      $msg = "gimmie dat data white boy";
+    }
+    
+    $request = array();
+    $request['type'] = "database";
+    $request['username'] = $username;
+    $request['password'] = $password;
+    $request['message'] = $msg;
+    $response = $client->send_request($request);
+    //$response = $client->publish($request);
+    
+    echo "client received response: ".PHP_EOL;
+    print_r($response);
+    echo "\n\n";
+    
+    echo $argv[0]." END".PHP_EOL;
     // check password
-    return true;
+    $hash = $response["pass"];
+    if (password_verify($password, $hash))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
     //return false if not valid
 }
 
