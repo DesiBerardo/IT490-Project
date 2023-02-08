dd9d68f9fc09dbede0178d48fa4bd1f2e4c174b8
Yash Patel <yashppatel01@gmail.com>
2023-02-08 17:19:36 -0500

    got response message to successfully show as a boolean flag

--- frontend/index.php
+++ frontend/index.php
@@ -1,7 +1,7 @@
 <?php
-require_once('rabbitmqphp_example/path.inc');
-require_once('rabbitmqphp_example/get_host_info.inc');
-require_once('rabbitmqphp_example/rabbitMQLib.inc');
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
 ?>
 
 <form method="POST">
@@ -33,17 +33,19 @@ if(isset($_POST["login"])){
     }
 
     $request = array();
-    $request['type'] = "Login";
+    $request['type'] = "login";
     $request['username'] = $username;
     $request['password'] = $password;
     $request['message'] = $msg;
     $response = $client->send_request($request);
 
-    echo "client received response: ".PHP_EOL;
-    print_r($response);
-    echo "\n\n";
-    
-    echo $argv[0]." END".PHP_EOL;
+    if(isset($response)){
+    echo($response);
+    }
+    else{
+        echo "Unknown if message delivered.";
+    }
+
 }
 ?>
 
