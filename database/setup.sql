38733f11351d58435fd70eafba342bc98a475565
Daniel Marriello <marriellodaniel@gmail.com>
2023-02-13 11:47:56 -0500

    Changed hash

--- database/setup.sql
+++ database/setup.sql
@@ -2,4 +2,4 @@ CREATE DATABASE userdb;
 USE userdb;
 CREATE TABLE users (id int NOT NULL AUTO_INCREMENT, username CHAR(12), pass CHAR(64), primary key(id));
 INSERT INTO users (username, pass)
-VALUES('testuser', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+VALUES('testuser', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5');
