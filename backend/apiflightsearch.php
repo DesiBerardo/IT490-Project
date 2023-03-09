5d30f3b3dff9378ace8fc9e1a3647b3ebc06cb1d
Yash Patel <yashppatel01@gmail.com>
2023-03-09 13:17:08 -0500

    almost done XHR to api call file

--- backend/apiflightsearch.php
+++ backend/apiflightsearch.php
@@ -28,59 +28,41 @@ curl_close($ch);
 $flight_search_url = "https://test.api.amadeus.com/v2/shopping/flight-offers";
 
 $body = [
-    'currencyCode' => 'USD',
-    'originDestinations' => [
-      0 => [
-        'id' => '1',
-        'originLocationCode' => 'BOS',
-        'destinationLocationCode' => 'MAD',
-        'departureDateTimeRange' => [
-          'date' => '2023-11-01',
-          'time' => '10:00:00',
-        ],
-      ],
-      
-    ],
-    'travelers' => [
-      0 => [
-        'id' => '1',
-        'travelerType' => 'ADULT',
-        'fareOptions' => [
-          0 => 'STANDARD',
-        ],
-      ],
-      1 => [
-        'id' => '2',
-        'travelerType' => 'CHILD',
-        'fareOptions' => [
-          0 => 'STANDARD',
-        ],
+  'currencyCode' => 'USD',
+  'originDestinations' => [
+    0 => [
+      'id' => '1',
+      'originLocationCode' => 'NYC',
+      'destinationLocationCode' => 'MAD',
+      'departureDateTimeRange' => [
+        'date' => '2023-11-01',
+        'time' => '10:00:00',
       ],
     ],
-    'sources' => [
-      0 => 'GDS',
+  ],
+  'travelers' => [
+    0 => [
+      'id' => '1',
+      'travelerType' => 'ADULT',
     ],
-    'searchCriteria' => [
-      'maxFlightOffers' => 2,
-      'flightFilters' => [
-        'cabinRestrictions' => [
-          0 => [
-            'cabin' => 'BUSINESS',
-            'coverage' => 'MOST_SEGMENTS',
-            'originDestinationIds' => [
-              0 => '1',
-            ],
-          ],
-        ],
-        'carrierRestrictions' => [
-          'excludedCarrierCodes' => [
-            0 => 'AA',
-            1 => 'TP',
-            2 => 'AZ',
+  ],
+  'sources' => [
+    0 => 'GDS',
+  ],
+  'searchCriteria' => [
+    'maxFlightOffers' => 2,
+    'flightFilters' => [
+      'cabinRestrictions' => [
+        0 => [
+          'cabin' => 'BUSINESS',
+          'coverage' => 'MOST_SEGMENTS',
+          'originDestinationIds' => [
+            0 => '1',
           ],
         ],
       ],
     ],
+  ],
 ];
 
 $headers = [
