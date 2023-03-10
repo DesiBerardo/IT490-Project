e1ebc48a2b3cacccab8f14cf0394fd68320a7ea4
Yash Patel <yashppatel01@gmail.com>
2023-03-10 18:54:37 -0500

    completed flight search results table building

--- frontend/flightsearch.php
+++ frontend/flightsearch.php
@@ -1,5 +1,114 @@
 <?php require(__DIR__."/nav.php"); ?>
 <link rel="stylesheet" href="styles.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+<script>
+
+function HandleAPIResponse(response)
+{
+    //api response was giving a random extra digit at the end of the response so I needed to slice it out
+    //console.log(response);
+    newresponse = response.slice(0,-1);
+    response = JSON.parse(newresponse);
+    console.log(response);
+
+//	document.getElementById("textResponse").innerHTML = response+"<p>";	
+	//document.getElementById("textResponse").innerHTML = "response: "+text+"<p>";
+    
+    //console.log("This is the server response: " +response);
+
+    //building the results table
+
+    document.getElementById("flight-results").innerHTML+= "<h2>Search Results</h2>";
+
+    document.getElementById("flight-results").innerHTML+="Total Options Returned: "+response["meta"]["count"]+"<br><br>";
+    for (let index = 0; index < response["meta"]["count"]; ++index){
+        
+        document.getElementById("flight-results").innerHTML+="\n\nOffer ID: " + response["data"][index]["id"];
+        document.getElementById("flight-results").innerHTML+=" Total Trip Cost: " + response["data"][index]["price"]["total"]+" "+response["data"][index]["price"]["currency"];
+
+        var table = document.createElement("table") ;
+        document.getElementById("flight-results").appendChild(table);
+        table.className="table";
+
+        var orderArrayHeader = ["Segment", "Flight Number", "Departure Time", "Arrival Time"];
+        var thead = document.createElement('thead');
+
+        table.appendChild(thead);
+
+        for (var i=0; i<orderArrayHeader.length; i++) {
+            thead.appendChild(document.createElement("th")).
+            appendChild(document.createTextNode(orderArrayHeader[i]));
+        }
+
+        
+        for (let indexItin = 0; indexItin < response["data"][index]["itineraries"].length; ++indexItin){
+            
+            for (let indexSeg = 0; indexSeg < response["data"][index]["itineraries"][indexItin]["segments"].length; ++indexSeg){
+                row = table.insertRow();
+                table.appendChild(row);
+                var cell = row.insertCell();
+                var newText = document.createTextNode(response["data"][index]["itineraries"][indexItin]["segments"][indexSeg]["departure"]["iataCode"]+"-"+response["data"][index]["itineraries"][indexItin]["segments"][indexSeg]["arrival"]["iataCode"]);
+                cell.appendChild(newText);
+
+                var cell = row.insertCell();
+                var newText = document.createTextNode(response["data"][index]["itineraries"][indexItin]["segments"][indexSeg]["carrierCode"]+" "+response["data"][index]["itineraries"][indexItin]["segments"][indexSeg]["number"]);
+                cell.appendChild(newText);
+
+                var cell = row.insertCell();
+                var departTiming = response["data"][index]["itineraries"][indexItin]["segments"][indexSeg]["departure"]["at"]
+                var newText = document.createTextNode(departTiming.slice(0,9)+" "+departTiming.slice(11,19));
+                cell.appendChild(newText);
+
+                var cell = row.insertCell();
+                var arrivalTiming = response["data"][index]["itineraries"][indexItin]["segments"][indexSeg]["arrival"]["at"]
+                var newText = document.createTextNode(arrivalTiming.slice(0,9)+" "+arrivalTiming.slice(11,19));
+                cell.appendChild(newText);
+
+            }
+        }
+    }
+}
+    
+    
+
+
+function SendBackendRequest()
+{   
+    const form = document.getElementById('flight-form');
+    var origin = String(form.elements["origin"].value);
+    var destination = String(form.elements["destination"].value);
+    var departDay = String(form.elements["dep-date"].value);
+    var departTime = String(form.elements["dep-time"].value);
+    var classType = String(form.elements["class-type"].value);
+
+        
+    console.log(origin, destination, departDay, departTime, classType);
+    
+    
+	var request = new XMLHttpRequest();
+	request.open("POST","apiflightsearch.php",true);
+	request.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	request.onreadystatechange= function()
+	{
+		
+		if ((this.readyState == 4)&&(this.status == 200))
+		{
+            //console.log(this.responseText);
+
+            HandleAPIResponse(this.responseText);
+
+		}
+	}
+	request.send("type=submit&origin="+origin+"&destination="+destination+"&depDay="+departDay+"&depTime="+departTime+"&class="+classType);
+}
+function sendLoginInfo() {
+        //not in use currently, code shifted to sendloginrequest
+        
+        SendLoginRequest(username , password);
+    }
+</script>
 
 <div id="search-form">
     <div id="header">
@@ -25,12 +134,12 @@
         <div id="flight-depart">
           <div class="info-box">
             <label for="">LEAVING FROM</label>
-            <input type="text" id="dep-from" />
+            <input type="text" id="origin" />
             <div id="depart-res"></div>
           </div>
           <div class="info-box" id="arrive-box">
             <label for="">ARRIVING AT</label>
-            <input type="text" id="dep-to" />
+            <input type="text" id="destination" />
             <div id="arrive-res"></div>
           </div>
         </div>
@@ -39,11 +148,14 @@
         <div id="flight-dates">
           <div class="info-box">
             <label for="">LEAVING ON</label>
-            <input type="date" id="leave-date" min="<?= date('Y-m-d'); ?>" />
+            <input type="date" id="dep-date" min="<?= date('Y-m-d'); ?>" />
+            <input type="time" id="dep-time"/>
           </div>
           <div class="info-box" id="return-box">
             <label for="">RETURNING ON</label>
             <input type="date" id="return-date" />
+            <input type="time" id="return-time"/>
+
           </div>
         </div>
         
@@ -87,9 +199,9 @@
       </form> 
       </div>
     </section>
-  
-    
-      
+</div>
+
+<div id="flight-results">
 </div>
 <script>
     document.getElementById("flight-form").addEventListener("submit", login);
@@ -99,7 +211,7 @@
     console.log('caught!');
 
     // more code goes here...
-    SendLoginRequest();
+    SendBackendRequest();
 }
 </script>
 
